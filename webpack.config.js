11d9f5ce1dfef15e014f609549134c838c3dc119
Sachin <sachin.g@cubiclogics.com>
2025-02-12 10:22:53 +0530

    returning babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-// module.exports = {
-//     module: {
-//       rules: [
-//         {
-//           test: /\.(js|jsx|ts|tsx)$/,
-//           exclude: /node_modules/,
-//           use: {
-//             loader: "babel-loader",
-//             options: {
-//               presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-//               plugins: ["@babel/plugin-proposal-private-property-in-object"]
-//             }
-//           }
-//         }
-//       ]
-//     }
-//   };
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+              plugins: ["@babel/plugin-proposal-private-property-in-object"]
+            }
+          }
+        }
+      ]
+    }
+  };
   
